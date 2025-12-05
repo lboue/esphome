ba63f1f9665e872d55b691dfde9fe87e5fc05234
Ludovic BOUÉ <lboue@users.noreply.github.com>
2025-12-05 23:13:07 +0000

    relative path

--- esphome/components/bmi270/bmi270_context_vendor.c
+++ esphome/components/bmi270/bmi270_context_vendor.c
@@ -2,4 +2,4 @@
   directly from esphome/components/bmi270/bmi2/*.c by the build system. */
 
 /* Compile vendor bmi270_context.c as a C translation unit */
-#include "/workspaces/esphome/esphome/components/bmi270/bmi2/bmi270_context.c"
+#include "bmi2/bmi270_context.c"
