15ca4684bec41492920db48f8f04480ebd7a7351
Ludovic BOUÉ <938089+lboue@users.noreply.github.com>
2026-08-04 07:55:16 +0200

    Refactor M5Stack NFC component: enhance setup and update methods, implement tag reading

--- esphome/components/m5stack_nfc/m5stack_nfc.cpp
+++ esphome/components/m5stack_nfc/m5stack_nfc.cpp
@@ -11,16 +11,48 @@ static const char *const TAG = "m5stack_nfc";
 void M5StackNFCComponent::setup() {
   ESP_LOGI(TAG, "Initializing M5Stack NFC");
 
-  // Communication avec le STM32 via I2C @ 0x50
+  uint8_t command = 0x00;
+  uint8_t response[4];
+
+  if (!this->write_read(&command, 1, response, sizeof(response))) {
+    ESP_LOGE(TAG, "M5Stack NFC not responding");
+    this->mark_failed();
+    return;
+  }
+
+  ESP_LOGI(TAG, "M5Stack NFC detected");
+  initialized_ = true;
 }
 
 void M5StackNFCComponent::update() {
-  // Poll NFC
+  if (!initialized_) {
+    return;
+  }
+
+  NFCTag tag;
+
+  if (this->read_tag(tag)) {
+    ESP_LOGI(TAG, "NFC tag detected");
+
+    ESP_LOGI(TAG, "UID length: %u", tag.uid_length);
+
+    for (uint8_t i = 0; i < tag.uid_length; i++) {
+      ESP_LOGI(TAG, "UID[%u]=0x%02X", i, tag.uid[i]);
+    }
+  }
 }
 
 void M5StackNFCComponent::dump_config() {
   ESP_LOGCONFIG(TAG, "M5Stack NFC");
   LOG_I2C_DEVICE(this);
+}
+
+bool M5StackNFCComponent::read_tag(NFCTag &tag) {
+  // TODO: implémenter le protocole I2C M5Stack NFC
+
+  tag.uid_length = 0;
+
+  return false;
 }
 
 }  // namespace m5stack_nfc
--- esphome/components/m5stack_nfc/m5stack_nfc.h
+++ esphome/components/m5stack_nfc/m5stack_nfc.h
@@ -6,15 +6,22 @@
 namespace esphome {
 namespace m5stack_nfc {
 
+struct NFCTag {
+  uint8_t uid[10];
+  uint8_t uid_length;
+};
+
 class M5StackNFCComponent : public PollingComponent, public i2c::I2CDevice {
  public:
   void setup() override;
   void update() override;
   void dump_config() override;
 
- protected:
-  bool send_command(uint8_t command);
-  bool read_response(uint8_t *data, size_t len);
+ private:
+  bool read_tag(NFCTag &tag);
+  bool read_version();
+
+  bool initialized_{false};
 };
 
 }  // namespace m5stack_nfc
